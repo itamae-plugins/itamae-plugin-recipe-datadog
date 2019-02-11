7995f14c73f1b72b024bf8e6ec5e368ae47835a8
Pocket7878 <poketo7878@gmail.com>
2019-02-11 20:27:38 +0900

    feature: Change Install target to v6 agent

--- lib/itamae/plugin/recipe/datadog/install.rb
+++ lib/itamae/plugin/recipe/datadog/install.rb
@@ -1,21 +1,23 @@
 node.reverse_merge!(
   datadog: {
-    install_only: true
+    install_only: true,
+    upgrade: false,
   }
 )
 
 execute 'download install script' do
-  command 'wget https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh'
+  command 'wget https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh'
 end
 
 execute "change install script's permission" do
-  command 'chmod 755 install_agent.sh'
+  command 'chmod 755 install_script.sh'
 end
 
 execute 'install datadog-agent' do
-  if node[:datadog][:install_only]
-    command "DD_API_KEY=#{node[:datadog][:api_key]} DD_INSTALL_ONLY=true ./install_agent.sh"
-  else
-    command "DD_API_KEY=#{node[:datadog][:api_key]} ./install_agent.sh"
-  end
+  options = {}
+  options['DD_API_KEY'] = node[:datadog][:api_key]
+  options['DD_INSTALL_ONLY'] = 'true' if node[:datadog][:install_only]
+  options['DD_UPGRADE'] = 'true' if node[:datadog][:upgrade]
+  option_str = options.map { |k, v| "#{k}=#{v}" }.joins(' ')
+  command "#{option_str} ./install_script.sh"
 end
