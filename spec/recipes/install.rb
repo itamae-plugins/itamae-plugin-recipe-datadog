b9d50ba79c31a57a6fdddc9ed72da96771fe88bb
sue445 <sue445@sue445.net>
2022-03-31 23:37:23 +0900

    Install groupadd in amazonlinux:2

--- spec/recipes/install.rb
+++ spec/recipes/install.rb
@@ -1,6 +1,10 @@
 case node[:platform]
 when "debian", "ubuntu"
   execute "apt-get update"
+
+when "amazon"
+  # `groupadd` isn't installed in https://hub.docker.com/_/amazonlinux
+  package "shadow-utils"
 end
 
 include_recipe "datadog::install"
